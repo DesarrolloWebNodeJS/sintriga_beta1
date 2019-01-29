ba0d3b795829b4f22ec65f61e24525d59a9da888
Vladimir Rodríguez <fizzvr@gmail.com>
2019-01-28 23:23:08 -0500

    > Show all text at ini. > 📲 Developer
    [Fix: ]

--- src/componentes/typing/index.js
+++ src/componentes/typing/index.js
@@ -4,15 +4,21 @@ import Typist from 'react-typist';
 
 const Escribiendo = ({ mensajes }) => {
   return (
-    <Typist cursor={{ show: false }}>
-      <span>Apasionados por la intriga,</span>
-      <Typist.Backspace count={11} delay={2000} />
-      <span>el crimen</span>
-      <Typist.Backspace count={0} delay={2000} />
-      <span> y la ciencia.</span>
-      <p> {mensajes[0]}</p>
-      <p> {mensajes[1]}</p>
-    </Typist>
+    <div style={{ color: '#FFC107'}}>
+      <Typist cursor={{ show: false }}>
+        <span>Apasionados por la intriga,</span>
+        <Typist.Backspace count={11} delay={2000} />
+        <span>el crimen</span>
+        <Typist.Backspace count={0} delay={2000} />
+        <span> y la ciencia.</span>
+      </Typist>
+      <Typist cursor={{ show: false }}>
+        <p> {mensajes[0]}</p>
+      </Typist>
+      <Typist cursor={{ show: false }}>
+        <p> {mensajes[1]}</p>
+      </Typist>
+    </div>
   );
 };
 
