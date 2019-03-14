61c0163e6665db37714b130cfdad52ade24f16ad
Vladimir Rodríguez <fizzvr@gmail.com>
2019-03-14 09:19:09 -0500

    > Update 404 > 📲 Developer
    [Fix: ]

--- src/componentes/404/index.js
+++ src/componentes/404/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import NoencontradoImagen from '../../recursos/404.png';
-import lander1 from '../../recursos/lander1.png';
-import lander2 from '../../recursos/lander2.png';
+import lander1 from '../../recursos/lander1.jpg';
+import lander2 from '../../recursos/lander2.jpg';
 import lander3 from '../../recursos/lander3.png';
 import Fondo from '../fondo';
 import Alerta from '../alerta';
 
 const Noencontrado = () => {
   return (
-    <Fondo imagenes={[NoencontradoImagen, lander1, lander2, lander3 ]}>
+    <Fondo imagenes={[lander1, lander2, lander3 ]}>
       <Alerta mensaje='Error 404' tipo='warning' />
     </Fondo>
   );
