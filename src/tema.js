dbf5675a7abc92d2d0e74d9370fe7e36c1c0eaab
Vladimir Rodríguez <fizzvr@gmail.com>
2019-05-30 07:29:10 -0500

    > remove cssbaseline > 📲 Developer
    [Fix: ]

--- src/tema.js
+++ src/tema.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-// import blueGrey from '@material-ui/core/colors/common';
-// import lightGreen from '@material-ui/core/colors/common';
 
 import common from '@material-ui/core/colors/common';
 import yellow from '@material-ui/core/colors/amber';
 // import Top from './top';
 // import Bot from './bot';
-// import AppWrapper from '../componentes/wrapper';
-import CssBaseline from '@material-ui/core/CssBaseline';
+// import CssBaseline from '@material-ui/core/CssBaseline';
 
 const theme = createMuiTheme({
   palette: {
@@ -45,7 +42,6 @@ function importar(Component) {
   function conPlantilla(props) {
     return (
       <MuiThemeProvider theme={theme}>
-        <CssBaseline />
         <Component {...props} />
       </MuiThemeProvider>
     );
