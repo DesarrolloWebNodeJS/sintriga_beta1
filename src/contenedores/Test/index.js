83146e21a969aa190f8c408175a4c0c44041b909
Vladimir Rodríguez <fizzvr@gmail.com>
2019-05-30 05:00:57 -0500

    > The test page with styles and coneccted to redux > 📲 Developer
    [Fix: ]

--- src/contenedores/Test/index.js
+++ src/contenedores/Test/index.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
+import tema from '../../tema';
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Alerta from '../../componentes/alerta';
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
 
 function mapStateToProps(state) {
   return {
@@ -10,14 +34,54 @@ function mapStateToProps(state) {
 
 class PruebaPagina extends Component {
   render() {
+    const { classes, mensaje, tipo } = this.props;
     return (
       <div>
-        <h1>Soy una PruebaPagina conectada.</h1>
+        <Helmet>
+          <title>Test | SINTRIGA</title>
+        </Helmet>
+        <div>
+          <AppBar position='static'>
+            <Toolbar>
+              <IconButton
+                aria-label='Menu'
+                className={classes.menuButton}
+                color='secondary'
+                >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                className={classes.flex}
+                color='secondary'
+                variant='title'
+                >
+                Test Pagina
+              </Typography>
+              <Button color='secondary'>Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <Typography
+                className={classes.flex}
+                variant='title'
+                >
+                Test Pagina
+              </Typography>
+        <Alerta mensaje={!mensaje ? 'Test Pagina' : ''} tipo={tipo} />
+        <Button color='primary' variant='contained'>
+          Primary
+        </Button>
+        <Button color='secondary' variant='contained'>
+          Secondary
+        </Button>
       </div>
     );
   }
 }
+PruebaPagina.propTypes = {
+  classes: PropTypes.object.isRequired,
+  tipo: PropTypes.string.isRequired,
+  mensaje: PropTypes.string.isRequired
+};
 
-export default connect(
-  mapStateToProps,
-)(PruebaPagina);
+export default tema(withStyles(styles)(connect(mapStateToProps)(PruebaPagina)));
