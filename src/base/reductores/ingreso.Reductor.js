74b3404a268d0c77cb7afd24d39965f7a3473845
Vladimir Rodríguez <fizzvr@gmail.com>
2019-06-11 00:23:59 -0500

    > Fix Ingreso reductor > 📲 Developer
    [Fix: ]

--- src/base/reductores/ingreso.Reductor.js
+++ src/base/reductores/ingreso.Reductor.js
@@ -2,16 +2,17 @@
 // import { FETCH_USER } from '../actions/types';
 import { SISTEMA_INGRESAR, SISTEMA_SALIR } from '../acciones/constantes';
 
-const initialState = {
-  auth: false
-};
+const user = localStorage.getItem('user');
+const initialState = user ? { auth: true, user } : { auth: false};
+
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case SISTEMA_INGRESAR:
       return {
         ...state,
-        auth: true
+        auth: true,
+        usuario: action.payload
       };
     case SISTEMA_SALIR:
       return {
