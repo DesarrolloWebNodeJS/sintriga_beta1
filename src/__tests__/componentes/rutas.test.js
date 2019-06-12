1de558a3291ae3927d6d4daf009b689050615679
Vladimir Rodríguez <fizzvr@gmail.com>
2019-06-12 17:08:20 -0500

    > Test ruta privada > 📲 Developer
    [Fix: ]

--- src/__tests__/componentes/rutas.test.js
+++ src/__tests__/componentes/rutas.test.js
@@ -4,7 +4,10 @@ import App from '../../contenedores/Rutas';
 
 describe('Rutas en SINTRIGA', () => {
   let rutas = shallow(<App />);
-  it('contiene las rutas', () => {
-    expect(rutas.find('Route')).toHaveLength(6);
+  it('contiene las rutas publicas', () => {
+    expect(rutas.find('Route')).toHaveLength(5);
+  });
+  it('contiene la ruta privada', () => {
+    expect(rutas.find('Privado')).toHaveLength(1);
   });
 });
