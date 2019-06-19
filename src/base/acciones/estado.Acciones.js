b603c106bc5452f90a9fc9d16f1d2401d11a1d6f
Vladimir Rodríguez <fizzvr@gmail.com>
2019-06-18 22:41:39 -0500

    > Si ya hay estado no vuelve a pedir a la API > 📲 Developer
    [Fix: ]

--- src/base/acciones/estado.Acciones.js
+++ src/base/acciones/estado.Acciones.js
@@ -7,34 +7,44 @@ import {
 
 export const _cmdgetEstado = idComponente => {
   return (dispatch, getState, apiSeries) => {
-    apiSeries
-      .get('/es')
-      .then(response => {
-        dispatch({
-          type: ESTADO_RECIBE,
-          payload: response.data
-        });
-        dispatch({
-          type: ALERTA_INFO, payload: 'Versión ' + response.data.appVersion
-        });
-      })
-      .catch(error => {
-        if (error.response) {
+    const estadoApp = getState().estado.catEstado;
+    const version = getState().estado.catEstado.appVersion;
+    dispatch({
+      type: ALERTA_LIMPIAR
+    });
+    if (estadoApp.length === 0) {
+      apiSeries
+        .get('/es')
+        .then(response => {
           dispatch({
-            type: ALERTA_ERROR,
-            payload: error.response.data.error.message
+            type: ESTADO_RECIBE,
+            payload: response.data
           });
-        } else if (error.request) {
           dispatch({
-            type: ALERTA_ERROR,
-            payload: error.message + ' 45' + idComponente
+            type: ALERTA_INFO,
+            payload: 'Versión ' + response.data.appVersion
           });
-        } else {
-          console.log(error.message);
-        }
-      });
+        })
+        .catch(error => {
+          if (error.response) {
+            dispatch({
+              type: ALERTA_ERROR,
+              payload: error.response.data.error.message
+            });
+          } else if (error.request) {
+            dispatch({
+              type: ALERTA_ERROR,
+              payload: error.message + ' 45' + idComponente
+            });
+          } else {
+            console.log(error.message);
+          }
+        });
+    } else {
       dispatch({
-        type: ALERTA_LIMPIAR
+        type: ALERTA_INFO,
+        payload: 'Versión ' + version
       });
+    }
   };
 };
