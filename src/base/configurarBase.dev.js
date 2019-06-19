6fbc85aa304f6a71ec5ed9e269e539b5872aa1e8
Vladimir Rodríguez <fizzvr@gmail.com>
2019-06-18 22:41:08 -0500

    > Set token in every request from axios > 📲 Developer
    [Fix: ]

--- src/base/configurarBase.dev.js
+++ src/base/configurarBase.dev.js
@@ -10,17 +10,26 @@ import rootReducer from './reductores';
 // export const history = createHistory();
 // NOTA: Apunta a la IP real del servidor donde se aloja express.
 const configurarBase = preloadedState => {
-  const axiosInstance = axios.create({
+  /*   const axiosInstance = axios.create({
     baseURL: 'http://192.168.100.95:5000'
+  }); */
+  const defaultOptions = {
+    baseURL: 'http://192.168.100.95:5000',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  const axiosInstance = axios.create(defaultOptions);
+  axiosInstance.interceptors.request.use(function(config) {
+    // const token = store.getState().session.token;
+    const token = localStorage.getItem('token');
+    config.headers.Authorization = token ? `${token}` : '';
+    return config;
   });
-
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-  const middleware = [
-    thunk.withExtraArgument(axiosInstance),
-    logg
-  ];
+  const middleware = [thunk.withExtraArgument(axiosInstance), logg];
   const store = createStore(
     rootReducer,
     preloadedState,
@@ -35,17 +44,3 @@ const configurarBase = preloadedState => {
   return store;
 };
 export default configurarBase;
-
-/*
-
-export default function configureStore() {
-  const routerMiddlewaree = routerMiddleware(history);
-
-  const middlewares = [routerMiddlewaree];
-  const store = createStore(
-    rootReducer(history),
-    compose(applyMiddleware(...middlewares))
-  );
-  return store;
-} */
-
