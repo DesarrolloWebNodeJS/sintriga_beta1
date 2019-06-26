508e587e0a2a698f2e50aa1ec4a5a94d4d0371f8
Vladimir Rodríguez <fizzvr@gmail.com>
2019-06-25 23:22:36 -0500

    > add interceptors > 📲 Developer
    [Fix: ]

--- src/base/configurarBase.prod.js
+++ src/base/configurarBase.prod.js
@@ -3,8 +3,18 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reductores';
 
-const axiosInstance = axios.create({
-  baseURL: 'http://192.168.100.95:5000'
+const defaultOptions = {
+  baseURL: 'http://192.168.100.95:5000',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+const axiosInstance = axios.create(defaultOptions);
+axiosInstance.interceptors.request.use(function(config) {
+  // const token = store.getState().session.token;
+  const token = localStorage.getItem('token');
+  config.headers.Authorization = token ? `${token}` : '';
+  return config;
 });
 
 const middleware = [
