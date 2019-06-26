2cb2297cee8e60ed4dd6a0a25ca2b8d98f92dda2
Vladimir Rodríguez <fizzvr@gmail.com>
2019-06-25 23:23:28 -0500

    > Fixes > 📲 Developer
    [Fix: ]

--- src/componentes/typing/index.js
+++ src/componentes/typing/index.js
@@ -5,15 +5,15 @@ import Typist from 'react-typist';
 
 const Escribiendo = ({ mensajes }) => {
   return (
-    <div style={{fontFamily: 'Rubik', textAlign: 'end'}}>
-      <Typography style={{color: '#f8f8f8'}} variant='h4'>
-      {'⭐'} Series de Intriga {'⭐'}
+    <div style={{fontFamily: 'Rubik'}}>
+      <Typography style={{color: '#000', textAlign: 'center'}} variant='h4'>
+      {'⭐'} Series Intriga {'⭐'}
       </Typography>
-      <Typography color='secondary' variant='h6'>
+      <Typography color='secondary' gutterBottom style={{textAlign: 'center'}} variant='h6'>
       <b>INTRIGA</b>, <b>CRIMEN</b> y <b>CIENCIA</b>
       </Typography>
 
-      <Typography color='secondary' gutterBottom variant='subtitle2'>
+      <Typography color='secondary' style={{textAlign: 'center'}} variant='subtitle2'>
       <Typist cursor={{ show: false }}>
         <p><span aria-label='tv' role='img'>📺</span> {mensajes[0]}</p>
         <p><span aria-label='primer' role='img'>🥇</span> {mensajes[1]}</p>
